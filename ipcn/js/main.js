9e08a6158c8d0e852f21b0f0f3b9da638f59a7fc
Vasya Pupkin <ybotyg@gmail.com>
2019-07-02 15:11:35 +0300

    first upload

--- ipcn/js/main.js
+++ ipcn/js/main.js
@@ -1,3 +1,37 @@
 $(document).ready(function () {
-   new WOW().init()
+  new WOW().init();
+  $(document).on("scroll", function () {
+    if
+    ($(document).scrollTop() > 250) {
+      $("header").css('padding', '3');
+    } else {
+      $("header").css('padding', '12');
+    }
+  });
+  $(".form-inline").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "/new.php", //Change
+      data: th.serialize()
+
+    }).done(function () {
+      $("#callback").fadeOut(100);
+      $(".success").addClass("visible");
+
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+        $(".success").removeClass("visible");
+
+      }, 3000);
+    });
+    return false;
+  });
+  $('#call-btn, .hide-btn').click(function () {
+    $('.overlay').fadeIn(500);
+  });
+  $('.closemodal').click(function () {
+    $('.overlay, #cookies').fadeOut(300);
+  });
 });
